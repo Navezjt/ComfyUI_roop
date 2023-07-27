a43d276912e818fb9cef96657177186358cb05de
ssit <ssit5568@gmail.com>
2023-07-27 18:32:46 -0400

    Add ability to change quantity of console messages

--- nodes.py
+++ nodes.py
@@ -2,6 +2,7 @@ import os
 from modules.processing import StableDiffusionProcessingImg2Img
 from scripts.faceswap import FaceSwapScript, get_models
 from utils import batch_tensor_to_pil, batched_pil_to_tensor, tensor_to_pil
+from logging_patch import apply_logging_patch
 
 
 def model_names():
@@ -19,6 +20,8 @@ class roop:
                 "swap_model": (list(model_names().keys()),),
                 # Comma separated face number(s)
                 "faces_index": ("STRING", {"default": "0"}),
+                # Allow user to change the logging amount, going from minimal to verbose
+                "console_logging_level": ([0, 1, 2],),
             }
         }
 
@@ -26,15 +29,17 @@ class roop:
     FUNCTION = "execute"
     CATEGORY = "image/postprocessing"
 
-    def execute(self, image, reference_image, swap_model, faces_index):
+    def execute(self, image, reference_image, swap_model, faces_index, console_logging_level):
+        apply_logging_patch(console_logging_level)
+
         script = FaceSwapScript()
         pil_images = batch_tensor_to_pil(image)
         source = tensor_to_pil(reference_image)
         p = StableDiffusionProcessingImg2Img(pil_images)
         script.process(
-            p=p, img=source, enable=True, faces_index=faces_index, model=swap_model, 
-            face_restorer_name=None, face_restorer_visibility=None, 
-            upscaler_name=None, upscaler_scale=None, upscaler_visibility=None, 
+            p=p, img=source, enable=True, faces_index=faces_index, model=swap_model,
+            face_restorer_name=None, face_restorer_visibility=None,
+            upscaler_name=None, upscaler_scale=None, upscaler_visibility=None,
             swap_in_source=True, swap_in_generated=True
         )
         result = batched_pil_to_tensor(p.init_images)
